7041b07ea2fee45f46ea83f333169847ee671059
Katchietsnouva <philipaswa01@gmail.com>
2023-09-20 15:57:31 +0300

    df
    df

--- lib/items_upload_screen.dart
+++ lib/items_upload_screen.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ItemsUploadScreen extends StatefulWidget {
-  const ItemsUploadScreen({super.key});
+  const ItemsUploadScreen({Key? key}) : super(key: key);
 
   @override
   State<ItemsUploadScreen> createState() => _ItemsUploadScreenState();
 }
 
-class _ItemsUploadScreenState extends State<ItemsUploadScreen> {
+class _ItemsUploadScreenState extends State<ItemsUploadScreen>
+{
+
+  //upload form screen
+
+  Widget uploadFormScreen()
+  {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          'Upload New Item',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
