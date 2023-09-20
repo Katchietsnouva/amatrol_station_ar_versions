1939dae9ced01892d6b2205ae60c8c2cb98ee76a
Katchietsnouva <philipaswa01@gmail.com>
2023-09-20 11:23:28 +0300

    Commit
    Learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amatrol_station_ar_versions/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const HomeScreen(),
     );
   }
 }
