ffb27d89096ccfc5ed5bc0766ca0d2d11e19f216
Katchietsnouva <philipaswa01@gmail.com>
2023-09-20 15:44:55 +0300

    sd
    sd

--- lib/items_upload_screen.dart
+++ lib/items_upload_screen.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class  extends StatefulWidget {
+  const ({super.key});
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
